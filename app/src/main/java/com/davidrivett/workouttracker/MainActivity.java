8a8232764018176b543003bf549e3be4362fa448
David Rivett <david.rivett@rosetta.com>
2015-06-23 12:10:28 -0700

    Initial Setup and playing with Material Design

--- app/src/main/java/com/davidrivett/workouttracker/MainActivity.java
+++ app/src/main/java/com/davidrivett/workouttracker/MainActivity.java
@@ -1,17 +1,54 @@
 package com.davidrivett.workouttracker;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.rootLayout)
+    CoordinatorLayout rootLayout;
+
+    @InjectView(R.id.fabBtn)
+    FloatingActionButton fabBtn;
+
+    @OnClick(R.id.fabBtn)
+    void onClick()
+    {
+        Snackbar.make(rootLayout, "Test Snack Bar", Snackbar.LENGTH_SHORT)
+                .setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+
+                    }
+                }).show();
+
+    }
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+
+        setSupportActionBar(toolbar);
     }
 
 
